6c112639176c20f48624a4ee0c52209574fd24b5
Aditya Aryan <40887649+AadiXAryan91@users.noreply.github.com>
2026-09-23 15:01:38 +0530

    Add: duplicate-integer - submission-3

---
+++ Data Structures & Algorithms/duplicate-integer/submission-3.swift
@@ -0,0 +1,32 @@
+class Solution {
+    func hasDuplicate(_ nums: [Int]) -> Bool {
+        return checkDuplicateFast(nums)
+        // return checkDuplicateSlow(nums)
+    }
+}
+
+func checkDuplicateFast(_ nums: [Int]) -> Bool {
+    var set = Set<Int>()
+
+        for i in nums {
+            if set.contains(i) {
+                return true
+            } else {
+                set.insert(i)
+            }
+        }
+
+    return false
+}
+
+func checkDuplicateSlow(_ nums: [Int]) -> Bool {
+    for i in 0..<nums.count {
+        for x in 0..<nums.count {
+            if (x != i) && (nums[x] == nums[i]) {
+                return true
+            }
+        }
+    }
+
+    return false
+}
